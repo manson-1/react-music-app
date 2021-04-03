88e91faa3c267f9f2ed9a70004594af1f4aebe54
Manuel <manuel.sonnleithner@gmail.com>
2021-04-03 19:58:18 +0200

    create first components

--- react-music-app/src/App.js
+++ react-music-app/src/App.js
@@ -1,6 +1,10 @@
+import Player from "./components/Player";
+import Song from "./components/Song";
+
 function App() {
   return <div className="App">
-    <h1>Music App</h1>
+    <Song />
+    <Player />
   </div>;
 }
 
