6e02403d20444c392b384ad9917b3857368e2847
Manuel <manuel.sonnleithner@gmail.com>
2021-04-08 23:50:53 +0200

    initial sass styling

--- react-music-app/src/App.js
+++ react-music-app/src/App.js
@@ -1,11 +1,14 @@
 import Player from "./components/Player";
 import Song from "./components/Song";
+import "./styles/app.scss";
 
 function App() {
-  return <div className="App">
-    <Song />
-    <Player />
-  </div>;
+  return (
+    <div className="App">
+      <Song />
+      <Player />
+    </div>
+  );
 }
 
 export default App;
