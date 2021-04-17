24e8a3aad7a8da4aa63f6465de16a4a6e9657398
Manuel <manuel.sonnleithner@gmail.com>
2021-04-17 19:15:14 +0200

    create library sidebar

--- react-music-app/src/App.js
+++ react-music-app/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import "./styles/app.scss";
+import Library from "./components/Library";
 import data from "./util";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         isPlaying={isPlaying}
         currentSong={currentSong}
       />
+      <Library songs={songs} />
     </div>
   );
 }
